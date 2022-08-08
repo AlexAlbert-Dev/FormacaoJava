43e4dd49ba14d332fd1cc950fadd27b067069d1f
Alex Albert Cosmo de Assis <108278362+AlexAlbert-Dev@users.noreply.github.com>
2022-08-08 14:41:35 -0300

    Parseando arquivo de leitura Scanner

--- src/br/com/alura/java/io/teste/TesteLeituraScanner.java
+++ src/br/com/alura/java/io/teste/TesteLeituraScanner.java
@@ -1,6 +1,7 @@
 package br.com.alura.java.io.teste;
 
 import java.io.File;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class TesteLeituraScanner {
@@ -12,6 +13,22 @@ public class TesteLeituraScanner {
 		while(scanner.hasNextLine()) {
 			String linha = scanner.nextLine();
 			System.out.println(linha);
+			
+			Scanner linhaScanner = new Scanner(linha);
+			linhaScanner.useLocale(Locale.US);
+			linhaScanner.useDelimiter(",");
+			
+			String valor1 = linhaScanner.next();
+			int valor2 = linhaScanner.nextInt();
+			int valor3 = linhaScanner.nextInt();
+			String valor4 = linhaScanner.next();
+			double valor5 = linhaScanner.nextDouble();
+			
+			System.out.println(valor1 + valor2 + valor3 + valor4 + valor5);
+			
+			linhaScanner.close();
+//			String[] valores = linha.split(",");
+//			System.out.println(valores[3]);
 		}
 		
 		scanner.close();
