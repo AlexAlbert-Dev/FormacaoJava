3791684614a971367746df93d7109ccef21f0b9c
Alex Albert Cosmo de Assis <108278362+AlexAlbert-Dev@users.noreply.github.com>
2022-08-08 09:44:28 -0300

    Saída com FileWriter

--- src/br/com/alura/java/io/teste/TesteEscritaFileWriter.java
+++ src/br/com/alura/java/io/teste/TesteEscritaFileWriter.java
@@ -2,23 +2,20 @@ package br.com.alura.java.io.teste;
 
 import java.io.BufferedWriter;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 
-public class TesteEscrita {
+public class TesteEscritaFileWriter {
 
 	public static void main(String[] args) throws IOException {
 		
-		// Fluxo de Saída com Arquivo
-		OutputStream fos = new FileOutputStream("lorem4.txt");
-		Writer osw = new OutputStreamWriter(fos);
-		BufferedWriter bw = new BufferedWriter(osw);
-		
+		BufferedWriter bw = new BufferedWriter(new FileWriter("lorem2.txt"));
 		bw.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod");
-		bw.newLine();
-		bw.newLine();
+		bw.write(System.lineSeparator());
+		bw.write(System.lineSeparator());
 		bw.write("fasdfasdfasdf");
 		
 		bw.close();
