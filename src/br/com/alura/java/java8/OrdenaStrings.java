6b96574568029dc5e3a037101a4e14d05430b920
Alex Albert Cosmo de Assis <108278362+AlexAlbert-Dev@users.noreply.github.com>
2022-08-18 13:19:45 -0300

    Implementando lambda na ordenacao de string

--- src/br/com/alura/java/java8/OrdenaStrings.java
+++ src/br/com/alura/java/java8/OrdenaStrings.java
@@ -1,7 +1,6 @@
 package br.com.alura.java.java8;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 public class OrdenaStrings {
@@ -13,23 +12,11 @@ public class OrdenaStrings {
 		palavras.add("editora casa do codigo");
 		palavras.add("caelum");
 		
-		Comparator<String> comparador = new ComparadorPorTamanho();
-		palavras.sort(comparador);
+		palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+		
 		System.out.println(palavras);
 		
 		palavras.forEach(s -> System.out.println(s));
 		
-	}
-}
-
-
-class ComparadorPorTamanho implements Comparator<String> {
-	@Override
-	public int compare(String s1, String s2) {
-		if(s1.length() < s2.length())
-			return -1;
-		if(s1.length() > s2.length())
-			return 1;
-		return 0;
 	}
 }
