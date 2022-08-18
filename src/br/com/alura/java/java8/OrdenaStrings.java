828489f450994461afe2d1d0beade628c81e676b
Alex Albert Cosmo de Assis <108278362+AlexAlbert-Dev@users.noreply.github.com>
2022-08-18 10:42:31 -0300

    Ordena lista de Strings pelo modo convencional

---
+++ src/br/com/alura/java/java8/OrdenaStrings.java
@@ -0,0 +1,19 @@
+package br.com.alura.java.java8;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class OrdenaStrings {
+
+	public static void main(String[] args) {
+		
+		List<String> palavras = new ArrayList<String>();
+		palavras.add("alura online");
+		palavras.add("editora casa do codigo");
+		palavras.add("caelum");
+		
+		Collections.sort(palavras);
+		System.out.println(palavras);
+	}
+}
