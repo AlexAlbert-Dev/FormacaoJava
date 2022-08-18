79062811bb48e359f86444a87379f78306ee5602
Alex Albert Cosmo de Assis <108278362+AlexAlbert-Dev@users.noreply.github.com>
2022-08-18 10:46:01 -0300

    Implementacao de ordenacao por tamanho

--- src/br/com/alura/java/java8/OrdenaStrings.java
+++ src/br/com/alura/java/java8/OrdenaStrings.java
@@ -2,6 +2,7 @@ package br.com.alura.java.java8;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class OrdenaStrings {
@@ -13,7 +14,19 @@ public class OrdenaStrings {
 		palavras.add("editora casa do codigo");
 		palavras.add("caelum");
 		
-		Collections.sort(palavras);
+		Comparator<String> comparador = new ComparadorPorTamanho();
+		Collections.sort(palavras, comparador);
 		System.out.println(palavras);
+	}
+}
+
+class ComparadorPorTamanho implements Comparator<String> {
+	@Override
+	public int compare(String s1, String s2) {
+		if(s1.length() < s2.length())
+			return -1;
+		if(s1.length() > s2.length())
+			return 1;
+		return 0;
 	}
 }
