cfa2beb04936e23cfbad8530971ce0f77887d09f
Alex Albert Cosmo de Assis <108278362+AlexAlbert-Dev@users.noreply.github.com>
2022-08-18 11:43:11 -0300

    Iterador foreach com interface consumer (Java8)

--- src/br/com/alura/java/java8/OrdenaStrings.java
+++ src/br/com/alura/java/java8/OrdenaStrings.java
@@ -1,9 +1,9 @@
 package br.com.alura.java.java8;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class OrdenaStrings {
 
@@ -18,9 +18,15 @@ public class OrdenaStrings {
 		palavras.sort(comparador);
 		System.out.println(palavras);
 		
-		for(String p : palavras) {
-			System.out.println(p);
-		}
+		Consumer<String> consumidor = new ImprimeNaLinha();
+		palavras.forEach(consumidor);
+	}
+}
+
+class ImprimeNaLinha implements Consumer<String> {
+	@Override
+	public void accept(String s) {
+		System.out.println(s);
 	}
 }
 
